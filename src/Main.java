65a9635a221f6cca39b89dc10b102a4c128d3f65
TungThanh <tung111104@st.vimaru.edu.vn>
2026-06-04 09:32:23 +0700

    homework

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,13 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Shape s1 = new Rectangle(2, 3);
+        Shape s2 = new Circle(1);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println("Diện tích hình chữ nhật: " + s1.area());
+        System.out.println("Diện tích hình tròn    : " + s2.area());
+
+        System.out.println("\n--- Kết quả chi tiết ---");
+        System.out.printf("Rectangle (2 x 3)     : %.2f%n", s1.area());
+        System.out.printf("Circle (r = 1)        : %.2f%n", s2.area());
     }
 }
